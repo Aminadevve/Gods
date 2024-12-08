34ce0af0af7a65b2dde6b19cebea2ccdafa15a7b
Aminadevve <pansheramina07@gmail.com>
2024-12-08 14:04:02 +0300

    addition

--- index.php
+++ index.php
@@ -18,8 +18,10 @@
                 echo "Вы ввели: $num1 и $num2\n";
                 break;
            case 2:
-               // Сложение (реализуем позже)
-               break;
+            $num1 = readline("Введите первое число: ");
+            $num2 = readline("Введите второе число: ");
+            $sum = $num1 + $num2;
+            echo "Сумма: $sum\n";               break;
            case 3:
                // Вычитание (реализуем позже)
                break;
